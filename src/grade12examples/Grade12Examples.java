686e1f485eef86be5ac4b83de962ff49f492b187
tarra7926 <tarra7926@HRH-IC0027324.wrdsb.ca>
2018-09-05 08:19:01 -0400

    Examples project creation

---
+++ src/grade12examples/Grade12Examples.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grade12examples;
+
+/**
+ *
+ * @author tarra7926
+ */
+public class Grade12Examples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
